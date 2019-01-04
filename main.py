46fd886a1e4f400a8dad24cdaf53af23224d44f1
jakub <rroott33@gmail.com>
2019-01-04 02:58:29 +0100

    beta

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import pandas as pd
 import sklearn.preprocessing
 from sklearn_pandas import DataFrameMapper
-from sklearn.decomposition import PCA
-
-import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import linear_model
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 
+arr = ['Temperature','Humidity','HumidityRatio','Light','CO2','Hour','Weekend']
 
-
-
-def prepareData(data):
-    time = data['date']
-    date = pd.to_datetime(time, format='%Y/%m/%d %H:%M:%S', utc=1)  # datetime
-    hours = date.dt.hour
-    weekend = date.dt.weekday.values
+def prepareData(dataFrame):
+    date = dataFrame['date']
+    dateTime = pd.to_datetime(date, format='%Y/%m/%d %H:%M:%S')
+    hours = dateTime.dt.hour
+    weekend = dateTime.dt.weekday.values
     weekend[weekend<5]= 0
     weekend[weekend>=5]= 1
-    Y = data['Occupancy'].values
-    data.drop({"date", "Occupancy"}, axis="columns", inplace=True)
+    Y = dataFrame['Occupancy']
+    dataFrame.drop({"date", "Occupancy"}, axis="columns", inplace=True)
     newAtributes = pd.DataFrame(data={'Hour': hours, 'Weekend': weekend},
                                 columns=['Hour', 'Weekend'])  # 1st column as index
-    X_df = data.join(newAtributes)
+    X_df = dataFrame.join(newAtributes)
+    X = X_df
     X = standarizeData(X_df)
     return X,Y
 
@@ -38,7 +35,6 @@ def standarizeData(data):
     return X
 
 
-
 datatraining = pd.read_csv('./occupancy_data/datatraining.txt', header = 0,delimiter = ",")
 datatest = pd.read_csv('./occupancy_data/datatest.txt', header = 0,delimiter = ",")
 datatest2 = pd.read_csv('./occupancy_data/datatest2.txt', header = 0,delimiter = ",")
@@ -48,27 +44,43 @@ X2,Y2 = prepareData(datatest)
 X3,Y3 = prepareData(datatest2)
 
 
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
 
 classifiers = [
     KNeighborsClassifier(5,algorithm='brute'),
     SVC(kernel="linear", C=0.025),  #c=0.025
     DecisionTreeClassifier(max_depth=4,presort=True,max_features=7),
-    MLPClassifier(alpha=1,solver='sgd' ,hidden_layer_sizes=(12,12),learning_rate='constant',learning_rate_init=0.0035,activation='logistic'),
+   MLPClassifier(alpha=1,solver='sgd' ,hidden_layer_sizes=(12,12),learning_rate='constant',learning_rate_init=0.0035,activation='logistic'),
 ]
 
-names = ["Nearest Neighbors", "Linear SVM","Decision Tree", "Neural Net"]
 
-from sklearn import metrics
+names = ["Nearest Neighbors", "Linear SVM","Decision Tree", "Neural Network"]
 
 for classifier, name in zip(classifiers, names):
     classifier.fit(X, Y)
-    predicts = classifier.predict(X2)
-    print(metrics.accuracy_score(Y2,predicts))
-  #  print('datatraining ' + name +':\t'+ str(classifier.score(X, Y)))
-    print('\ndatatest ' + name +':\t'+ str(classifier.score(X2, Y2)))
-    print('datatest2 '+ name +':\t'+ str(classifier.score(X3, Y3)))
 
+    print('datatest ' + name +' accuracy:\t'+ str(classifier.score(X2, Y2))+"%")
+    print('datatest2 '+ name +' accuracy:\t'+ str(classifier.score(X3, Y3))+"%\n")
+
+
+
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+
+for classifier, name in zip(classifiers, names):
+    for i in range(1,7):
+        features = i
+        sfs = SFS(classifier,
+                  k_features=features,
+                  forward=True,
+                  floating=False,
+                  scoring='accuracy',
+                  cv=4,
+                  n_jobs=-1)
+        sfs = sfs.fit(X, Y)
+
+
+
+        print('\nSequential Forward Selection (k='+str(features)+', '+name+' ):')
+        for i in sfs.k_feature_idx_:
+            print(arr[i])
+        print('CV Score:')
+        print(sfs.k_score_)
